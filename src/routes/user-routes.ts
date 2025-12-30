f5ccc78a2e7b84b774cbda4b3eab62512584aadf
Majid Ali <itsmajidali129@gmail.com>
2025-12-30 11:08:15 +0500

    feat: follow unfollow accounts

--- src/routes/user-routes.ts
+++ src/routes/user-routes.ts
@@ -1,10 +1,19 @@
+import { followUser, getCurrentUserFollowers, getCurrentUserFollowings, unfollowUser } from "@/controllers/follow-account-controller";
+import { verifyRequest } from "@/middleware/verify-request";
 import { Router } from "express";
 
 
 const router = Router()
 
 
+router.use(verifyRequest)
 router.route('/:id/profile')
+router.route('/:id/follow').post(followUser).delete(unfollowUser);
+
+router.get('/:id/followers', getCurrentUserFollowers)
+router.get('/:id/followings', getCurrentUserFollowings)
+
+//TODO: Add routes to get another user's followers and followings
 
 
 export { router as userRouter }
